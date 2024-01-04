90b96fbea1818c499630c5d01dccaf3479685a8f
Kai17-a <k.s25skt@gmail.com>
2024-01-04 10:34:10 +0900

    chore: 関数コメント追加

--- discord-app/interaction/app.py
+++ discord-app/interaction/app.py
@@ -4,7 +4,10 @@ import json
 import discord_interactions
 from discord_interactions import InteractionType, InteractionResponseType
 
-def verify_request(event) -> bool:
+logging.basicConfig(level=logging.INFO, format="[%(levelname)s] %(asctime)s %(message)s")
+logger = logging.getLogger()
+
+def verify_request(event: dict) -> bool:
     """
     リクエストの署名検証
     
@@ -16,21 +19,41 @@ def verify_request(event) -> bool:
     Returns
     -------
     verify_result : bool
-        処刑検証結果
+        処理検証結果
     """
-    headers = event['headers']
     body = event['body']
+    headers = event['headers']
     signature = headers['x-signature-ed25519']
     timestamp = headers['x-signature-timestamp']
     publicKey = os.getenv('DiscordPublicKey')
     if not body or not signature or not timestamp or not publicKey:
+        logger.info("リクエスト情報が存在しません")
         return False
-    return discord_interactions.verify_key(bytes(body, 'utf-8'), signature, timestamp, publicKey)
+    if discord_interactions.verify_key(bytes(body, 'utf-8'), signature, timestamp, publicKey):
+        return True
+    else:
+        logger.error("リクエスト検証に失敗しました")
+        return False
 
-def handle_interaction(interaction):
+def handle_interaction(interaction: dict) -> dict:
+    """
+    interaction の処理
+    ここでslash commandsの振分け＆処理を行う
+    
+    Parameters
+    ----------
+    interaction : dict
+        AWS Lambda イベント引数 body
+    
+    Returns
+    -------
+    content : dict
+        discordレスポンス
+    """
     if interaction['type'] is InteractionType.APPLICATION_COMMAND:
-        data = interaction['data']
-        if data['name'] == 'hello':
+        # discordからのリクエストがslash commandsの場合
+        command = interaction['data']
+        if command['name'] == 'hello':
             return {
                 "type": InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
                 "data": {
@@ -43,8 +66,7 @@ def handle_interaction(interaction):
         }
 
 def lambda_handler(event, context):
-    logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s:%(name)s - %(message)s")
-    logging.info(event['body'])
+    logger.info(event['body'])
 
     if not verify_request(event):
         return {
